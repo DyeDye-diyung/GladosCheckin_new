6975ee49123b12fba66929454787e41a074f65c5
DyeDye-diyung <wshym11@gmail.com>
2026-02-10 16:36:28 +0800

    Add wxpush API and WeChat configuration parameters
    Added wxpush configuration parameters for API and WeChat.

--- configNew.py
+++ configNew.py
@@ -13,3 +13,14 @@ cookies = [
     f"koa:sess={os.environ['KOA_SESS'].strip()}; koa:sess.sig={os.environ['KOA_SESS_SIG'].strip()}",
     f"koa:sess={os.environ['KOA_SESS_2'].strip()}; koa:sess.sig={os.environ['KOA_SESS_SIG_2'].strip()}",
 ]
+
+# go-wxpush相关参数
+
+# 你的自建服务地址，记得带上 /wxsend
+wxpush_api_url = os.environ["WXPUSH_API_URL"].strip()
+
+# 微信测试号参数
+wxpush_appid = os.environ["WXPUSH_APPID"].strip()
+wxpush_secret = os.environ["WXPUSH_SECRET"].strip()
+wxpush_userid = os.environ["WXPUSH_USERID"].strip()
+wxpush_template_id = os.environ["WXPUSH_TEMPLATE_ID"].strip()
